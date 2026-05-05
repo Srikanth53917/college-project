9a604715f807b62500e266006e496c891c947bc8
Srikanth53917 <srikanthchityala168@gmail.com>
2026-05-05 16:22:27 +0530

    Update stock_prediction.py

--- prediction/stock_prediction.py
+++ prediction/stock_prediction.py
@@ -1,21 +1,28 @@
 import numpy as np
+import pandas as pd
+import joblib
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 def train_stock_model(data):
-    data = data.reset_index()
+    data = data.copy()
+    data['Prediction'] = data['Close'].shift(-1)
+    data.dropna(inplace=True)
 
-    data["Day"] = np.arange(len(data))
+    X = data[['Close']]        # 2D
+    y = data['Prediction']     # 1D
 
-    X = data[["Day"]]
-    y = data["Close"]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
 
     model = LinearRegression()
-    model.fit(X, y)
+    model.fit(X_train, y_train)
 
-    return model
+    joblib.dump(model, "models/stock_model.pkl")
+    return model, X_test, y_test
 
-
-def predict_next_day(model, data):
-    next_day = len(data)
-    prediction = model.predict([[next_day]])
-    return float(prediction[0])
+def predict_next_day(model, last_close):
+    input_df=pd.DataFrame([[last_close]],columns=['Close'])
+    return model.predict(input_df)[0]
+    
